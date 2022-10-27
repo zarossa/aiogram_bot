9e6a005838ef1351a21541095f0040c6c042c8a2
zarossa <mr.kirarik@yandex.ru>
2022-10-27 18:18:04 +0700

    add a cenzor

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,7 +1,9 @@
+import string
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import os
+import os, json
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
@@ -48,9 +50,9 @@ async def pizza_place_command(message: types.Message):
 
 @dp.message_handler()
 async def echo_send(message: types.Message):
-    # await message.answer(message.text)
-    # await message.reply(message.text)
-    await bot.send_message(message.from_user.id, message.text)
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split()}.intersection(set(json.load(open('cenz.json')))) != set():
+        await message.reply('Маты запрещены')
+        await message.delete()
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
