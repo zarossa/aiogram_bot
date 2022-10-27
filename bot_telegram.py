2df09baf17a53c4b3ab8a56a31f82e9cd48acbe9
zarossa <mr.kirarik@yandex.ru>
2022-10-27 17:45:07 +0700

    add starting commands

--- bot_telegram.py
+++ bot_telegram.py
@@ -6,7 +6,44 @@ import os
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
-###
+
+async def on_startup(_):
+    print('Bot is online')
+
+
+'''Клиентская часть'''
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        # await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/zarossa_test1_bot')
+
+
+@dp.message_handler(commands=['Режим_работы'])
+async def pizza_open_command(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Пн-ВС круглосуточно')
+        # await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/zarossa_test1_bot')
+
+
+@dp.message_handler(commands=['Расположение'])
+async def pizza_place_command(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'ул. Юрная 11')
+        # await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/zarossa_test1_bot')
+
+
+'''Админская часть'''
+
+'''Общая часть'''
 
 
 @dp.message_handler()
@@ -16,4 +53,4 @@ async def echo_send(message: types.Message):
     await bot.send_message(message.from_user.id, message.text)
 
 
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
